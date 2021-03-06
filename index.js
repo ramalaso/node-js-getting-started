5d4a1f7ad099dc688fa1290b625436e821dc784b
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-03-06 08:19:45 -0400

    Added calculator app

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const {calculateRate} = require('./utils/price_calculator')
 app.use(express.static(path.join(__dirname, 'public')))
 app .set('views', path.join(__dirname, 'views'))
 app .set('view engine', 'ejs')
+
 app .get('/', (req, res) => res.render('pages/index'))
 
 app.get('/getRate', (req, res)=>{
