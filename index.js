f005d72098fe8fb27a65aeb418d27c7ea7e7112a
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-03-13 17:12:32 -0400

    Updated application

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const url = require('url')
 const path = require('path')
-const PORT = process.env.PORT || 5000
 const app = express()
 const {calculateRate} = require('./utils/price_calculator')
+const routes = require('./routes/index');
 
+require('dotenv').config();
+const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'public')))
 app .set('views', path.join(__dirname, 'views'))
 app .set('view engine', 'ejs')
+
 
 app .get('/', (req, res) => res.render('pages/index'))
 
---
+++ routes/index.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var router = express.Router();
+
+//pg config
+var pg = require('pg');
+var conString = process.env.DATABASE_URL;
+
+//Suppliers
+//get all Suppliers
+router.get('/suppliers', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('SELECT * FROM suppliers', function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+    });
+  });
+});
+//post supplier
+router.post('/suppliers', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('INSERT INTO suppliers(supplier_address ,supplier_name ,supplier_contact , supplier_details ) VALUES($1, $2, $3, $4) returning id', [req.body.address, req.body.name, req.body.contact, req.body.details ], function(err, result) {
+      done();
+      if(err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+    });
+  });
+});
+//get one supplier
+router.get('/suppliers/:id', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('SELECT * FROM suppliers WHERE id = $1', [req.params.id], function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+    });
+  });
+});
+// update supplier
+router.put('/suppliers/:id', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    //compare with .compareSync(req.body.data.attributes.password, storedPW)
+    client.query('UPDATE suppliers SET supplier_address = $2, supplier_name = $3, supplier_contact = $4, supplier_details = $5   WHERE id = $1', [req.params.id, req.body.address, req.body.name, req.body.contact, req.body.details ], function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+    });
+  });
+});
+//delete one supplier
+router.delete('/suppliers/:id', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+     console.log(conString)
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    console.log("connected to database");
+    client.query('DELETE FROM suppliers WHERE id = $1',[req.params.id], function(err, result) {
+      done();
+      if (err) {
+        return console.error('error running query', err);
+      }
+      res.send(result);
+    });
+  });
+});
+
+module.exports = router;
