85b0dfd6d6aa0aa958e3638ad5b912e51d39ebf7
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-03-13 17:24:43 -0400

    Update

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
-const url = require('url')
 const path = require('path')
 const app = express()
-const {calculateRate} = require('./utils/price_calculator')
-const routes = require('./routes/index');
+const routes = require('./routes/suppliers');
 
 require('dotenv').config();
 const PORT = process.env.PORT || 5000
@@ -13,12 +11,5 @@ app .set('views', path.join(__dirname, 'views'))
 app .set('view engine', 'ejs')
 
 app.use('/suppliers', routes)
-app .get('/', (req, res) => res.render('pages/index'))
-app.get('/getRate', (req, res)=>{
-  const queryObject = url.parse(req.url,true).query;
-  console.log(queryObject.type, queryObject.weight)
-  console.log(calculateRate(queryObject.type, queryObject.weight))
-  res.render('pages/response', {price: calculateRate(queryObject.type, queryObject.weight)})
-})
 
 app .listen(PORT, () => console.log(`Listening on ${ PORT }`))
