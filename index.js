0f1587419a87d0066b5a3c52f82f62f23b4476e9
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-03-13 20:56:50 -0400

    Update

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
@@ -28,3 +27,4 @@ express()
       res.send("Error " + err);
     }
   })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
