8b6eb32c25721a63ff4ba8c124e8822328ddab5e
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-03-13 21:06:32 -0400

    Update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ express()
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
+      const result = await client.query('SELECT * FROM suppliers');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
