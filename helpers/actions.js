fa8f1e7dbbaf148a6463899a9941413b623b547e
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-03-20 17:17:40 -0400

    Update

--- helpers/actions.js
+++ helpers/actions.js
@@ -4,7 +4,11 @@ function getSuppliers() {
     axios.get('https://gentle-anchorage-20332.herokuapp.com//api/v1/suppliers')
   .then(function (response) {
     // handle success
-    console.log(response);
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
   })
   .catch(function (error) {
     // handle error
