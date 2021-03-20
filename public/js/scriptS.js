b87ea1f104a93c1f495c6f801082165da82c0409
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-03-20 17:42:33 -0400

    Update

--- public/js/scriptS.js
+++ public/js/scriptS.js
@@ -1,19 +1,10 @@
-// const axios = require('axios');
-// function getSuppliers() {
-//   axios.get('/api/v1/suppliers')
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-// }
-// getSuppliers();
-fetch('https://gentle-anchorage-20332.herokuapp.com/api/v1/suppliers')
-.then(response => response.json())
-.then(data => console.log(data));
+function getSuppliers() {
+  fetch('https://gentle-anchorage-20332.herokuapp.com/api/v1/suppliers')
+  .then(response => response.json())
+  .then(data => data);
+}
+const suppliers = getSuppliers();
+console.log(suppliers);
 const txtAddName = document.getElementById("addName");
 const txtAddQuantity = document.getElementById("addQuantity");
 const txtAddPrice = document.getElementById("addPrice");
