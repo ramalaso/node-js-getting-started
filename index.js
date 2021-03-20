5042d09c97bc3c1a739bb8fc49799a1f30f17306
Raul Laredo Soria <raul.laredo@jalasoft.com>
2021-03-20 16:26:04 -0400

    Update UI

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app .set('views', path.join(__dirname, 'views'))
 app .set('view engine', 'ejs')
 
 app.get('/', (req, res)=>{
-  res.render('index')
+  res.render('pages/index')
 })
 
 app.use('/suppliers', suppliers)
